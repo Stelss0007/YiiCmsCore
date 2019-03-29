b2489ba966120b342619326125e07a867878720f
Ruslan <stelss1986@gmail.com>
2019-03-29 10:37:27 +0200

    get object name for page

--- src/CmsController.php
+++ src/CmsController.php
@@ -6,5 +6,21 @@ use yii\web\Controller;
 
 class CmsController extends Controller
 {
+    public function getObjectName()
+    {
+        $objectName =  $this->module->id . '::' . $this->id . '::' . $this->action->id;
 
+        if ($this->actionParams) {
+            foreach ($this->actionParams as $key => $value) {
+                $objectName .= '::' . $key . '::' . $value;
+            }
+        }
+
+        return $objectName;
+    }
+
+    protected function getAccess()
+    {
+
+    }
 }
